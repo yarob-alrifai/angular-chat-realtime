a6a62a3c8a72d420dd6104df19725d52e6112072
yarob <yalrifai41@gmail.com>
2024-06-26 23:37:04 +0300

    add commints to the home files

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {
   Observable,
   combineLatest,
